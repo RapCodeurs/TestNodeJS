6816d0f2c4dd5591b7147e8b6bfaa9f80dcad655
RapCodeurs <maureljoumessi9@gmail.com>
2023-12-13 20:41:09 +0100

    Test avec Node JS

---
+++ script.js
@@ -0,0 +1,18 @@
+let hello = () => {
+    console.log('Bonjour mes amis je sérai un expert en Node.js')
+};
+
+
+hello();
+
+/*const assert = require('assert');
+
+
+describe('Array', function() {
+    describe('IndexOf', function() {
+        it('should return -1 when the value is not prevent', function() {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});*/
+
